ab926a8e27e3f6ff4560fc40f09bc64162592f77
Harish <harish_balamurugan@yahoo.com.sg>
2018-12-18 16:44:59 +0800

    Convert bytecode to image without scaling bytecode

--- application_of_ai_in_cyber_security/convert_bytes_to_image.py
+++ application_of_ai_in_cyber_security/convert_bytes_to_image.py
@@ -1,16 +1,33 @@
-import os
+import os, array, itertools
 from PIL import Image
-import array
 import numpy as np
 
 def convert(filename):
     try:
-        with open(filename, 'rb') as f:
-            size = os.path.getsize(filename)
+        with open(filename, 'r') as f:
+            arr = []
+            for line in f:
+                vals = line.split()
+                del vals[0]
+                arr.append(vals)
+            
+            max_len = max([len(vals) for vals in arr])
+            
+            new_arr = []
+            for vals in arr:
+                new_arr.append([val.replace('?', '0') for val in vals])
+            
+            for vals in new_arr:
+                if '?' in vals:
+                    print(vals)
+            
+            hexstring = ''.join(list(itertools.chain.from_iterable(new_arr)))
+            
+            byte_arr = bytearray.fromhex(hexstring)
             width = 1024
-            rem = size % width
-            byte_arr = array.array('B')
-            byte_arr.fromfile(f, size - rem)
+            rem = len(byte_arr) % width
+            byte_arr_len = len(byte_arr) - rem
+            byte_arr = byte_arr[:byte_arr_len]
             byte_arr = np.asarray(byte_arr)
             np_arr = np.reshape(byte_arr, (len(byte_arr)//width, width))
             np_arr = np.uint8(np_arr)
